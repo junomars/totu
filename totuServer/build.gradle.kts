ec344dacef9e5c7b6cb8a3800631262bd137d1e8
juno <jonathan_nuno@outlook.com>
2024-09-19 20:17:35 +0000

    Remove Kotlin JVM toolchain configuration
    This commit removes the Kotlin JVM toolchain setup from the build.gradle.kts file. The `jvmToolchain` configuration, along with source and target compatibility settings, have been deleted to run according to the pipeline settings. It might make more sense to leave this to the build settings in the individual project. I'll revisit later.

--- totuServer/build.gradle.kts
+++ totuServer/build.gradle.kts
@@ -77,10 +77,3 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
-kotlin {
-    jvmToolchain(17)
-}
-tasks.withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
-}
