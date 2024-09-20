4fda51623cd2c524796512e17ada9fc5a9b757fe
juno <jonathan_nuno@outlook.com>
2024-09-20 03:29:20 +0000

    Add Kotlin and Ktor plugin versions in root gradle file
    Updated Kotlin plugin to version 1.9.22 and Ktor plugin to version 2.3.9. This ensures compatibility with the latest features and improvements in the respective libraries.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") apply false
-    id("io.ktor.plugin") apply false
+    kotlin("jvm") version "1.9.22" apply false
+    id("io.ktor.plugin") version "2.3.9" apply false
 }
 
 group = "space.junodev"
